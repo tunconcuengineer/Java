a3699acb94c64ca13572e25357b1ee1da1ff2dcd
tunconcuengineer <tunconcuengineer@gmail.com>
2023-09-24 15:55:55 +0300

    Create Main.java

---
+++ StringBuffBuild/Main.java
@@ -0,0 +1,35 @@
+package StringBuffBuild;
+
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder stb = new StringBuilder();
+        stb.append("Hello World!");
+        System.out.println(stb.reverse());
+        stb.reverse();
+        stb.deleteCharAt(1);
+        System.out.println(stb);
+        stb.delete(2, 4);
+        System.out.println(stb);
+        stb.insert(1, "e");
+        System.out.println(stb);
+        stb.insert(3, "l");
+        stb.insert(4, "o");
+        System.out.println(stb);
+        System.out.println(stb.capacity());
+        stb.setCharAt(4, 'c');
+        System.out.println(stb);
+        stb.deleteCharAt(4);
+        System.out.println(stb);
+        String s = stb.substring(0, 3);
+        System.out.println(s);
+        System.out.println(stb);
+        
+        StringBuffer stbuff = new StringBuffer("Hello There");
+        System.out.println(stbuff);
+        // StringBuffer is thread safe whereas StringBuilder is not
+        // StringBuilder is faster and preferred over StringBuilder in single-thread programs
+        
+
+
+    }
+}
